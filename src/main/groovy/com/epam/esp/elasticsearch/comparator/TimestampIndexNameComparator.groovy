558f27301505b1666bc21278a7be5dde3c1315d4
MINSK\Maksim_Lauryk <maksim.lauryk@burberry.com>
2018-05-03 14:21:15 +0300

    fix for comparator

--- src/main/groovy/com/epam/esp/elasticsearch/comparator/TimestampIndexNameComparator.groovy
+++ src/main/groovy/com/epam/esp/elasticsearch/comparator/TimestampIndexNameComparator.groovy
@@ -27,21 +27,30 @@ class TimestampIndexNameComparator implements Comparator<String> {
 
     @Override
     int compare(String o1, String o2) {
+        try {
+            if (!compatibilityCheck(o1, o2)) return 0
 
-        if(!compatibilityCheck(o1, o2)) return 0
+            long ts1 = getTimestamp(o1) + offset
+            long ts2
+            if (o2 == null) ts2 = System.currentTimeMillis() else ts2 = getTimestamp(o2)
 
-        long ts1 = getTimestamp(o1) + offset
-        long ts2
-        if (o2 == null) ts2 = System.currentTimeMillis() else ts2 = getTimestamp(o2)
-
-        if (ts1 < ts2) return -1
-        if (ts1 > ts2) return 1
-
-        return 0
+            if (ts1 < ts2) return -1
+            if (ts1 > ts2) return 1
+            return 0
+        } catch (Exception e) {
+            return 0
+        }
     }
 
     private boolean compatibilityCheck(String o1, String o2) {
-        return o1.substring(0, o1.lastIndexOf(delimiter)).equals(o2.substring(0, o2.lastIndexOf(delimiter)))
+        int d1 = o1.lastIndexOf(delimiter)
+        int d2 = o2.lastIndexOf(delimiter)
+
+        if (d1 > 0 && d2 > 0) {
+            return o1.substring(0, d1).equals(o2.substring(0, d2))
+        } else {
+            return false
+        }
     }
 
     private long getTimestamp(String name) {
